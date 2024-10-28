34daab0b81e8646f78558644d0d6ffde49abcccd
EL REY UNO <EddieZeboSlightWindow@gmail.com>
2024-10-28 15:20:21 -0400

    Implemented Page To Apply For Credit and Updated E-Mail Addy In Index

--- js/qualify.js
+++ js/qualify.js
@@ -1,139 +1,164 @@
-const emailInput = document.getElementById('myEmail');
-const reEnterEmailInput = document.getElementById('myReEmail');
-const firstNameInput = document.getElementById('myFirstName');
-const lastNameInput = document.getElementById('myLastName');
-const cityInput = document.getElementById('myCity');
-const stateInput = document.getElementById('myState');
-const zipInput = document.getElementById('myZip');
-const grossIncomeInput = document.getElementById('myGross');
-const ssnInput = document.getElementById('mySsn');
-const applyButton = document.getElementById('myApply');
-const resetButton = document.getElementById('resetButton');
-const resultsTable = document.getElementById('resultsTable');
-const applyForCreditCheckbox = document.getElementById('applyForCredit');
-const resultsContainer = document.getElementById('resultsContainer');
+document.addEventListener('DOMContentLoaded', function () {
+   const form = document.querySelector('.form-container');
+   const emailInput = document.getElementById('creditEmailInput');
+   const confirmEmailInput = document.getElementById('confirmCreditEmailInput');
+   const firstNameInput = document.getElementById('creditFirstNameInput');
+   const lastNameInput = document.getElementById('creditLastName');
+   const cityInput = document.getElementById('creditCity');
+   const stateInput = document.getElementById('creditState');
+   const zipInput = document.getElementById('creditZip');
+   const grossIncomeInput = document.getElementById('creditGross');
+   const ssnInput = document.getElementById('creditSsn');
+   const applyButton = document.getElementById('creditApply');
+   const resetButton = document.getElementById('creditResetButton');
+   const resultsTable = document.getElementById('creditResultsTable');
+   const applyForCreditCheckbox = document.getElementById('applyForCredit');
+   const resultsContainer = document.getElementById('creditResultsContainer');
 
-function displayError(inputField, message) {
-  const errorElement = document.createElement('span');
-  errorElement.classList.add('error-message');
-  errorElement.textContent = message;
+   function displayError(inputField, message) {
+      const errorElement = document.createElement('span');
+      errorElement.classList.add('error-message');
+      errorElement.textContent = message;
 
-  inputField.parentElement.insertBefore(errorElement, inputField.nextSibling);
-}
+      inputField.parentElement.insertBefore(errorElement, inputField.nextSibling);
+   }
 
-function clearErrors() {
-  const errorElements = document.querySelectorAll('.error-message');
-  errorElements.forEach(errorElement => errorElement.remove());
-}
+   function clearErrors() {
+      const errorElements = document.querySelectorAll('.error-message');
+      errorElements.forEach(errorElement => errorElement.remove());
+   }
 
-const requiredFields = [
-  firstNameInput,
-  lastNameInput,
-  cityInput,
-  stateInput,
-  zipInput,
-  grossIncomeInput,
-  ssnInput,
-  applyForCreditCheckbox
-];
+   function clearForm() {
+      emailInput.value = '';
+      confirmEmailInput.value = '';
+      firstNameInput.value = '';
+      lastNameInput.value = '';
+      cityInput.value = '';
+      stateInput.value = '';
+      zipInput.value = '';
+      grossIncomeInput.value = '';
+      ssnInput.value = '';
+      applyForCreditCheckbox.checked = false;
 
-function validateForm() {
-  clearErrors();
+      clearErrors();
 
-  resultsTable.querySelector('tbody').innerHTML = '';
+      resultsTable.innerHTML = '';
 
-  const formData = {
-    email: emailInput.value.trim(),
-    reEnterEmail: reEnterEmailInput.value.trim(),
-    firstName: firstNameInput.value.trim(),
-    lastName: lastNameInput.value.trim(),
-    city: cityInput.value.trim(),
-    state: stateInput.value.trim(),
-    zip: zipInput.value.trim(),
-    grossIncome: parseFloat(grossIncomeInput.value.trim()),
-    ssn: ssnInput.value.trim(),
-    applyForCredit: applyForCreditCheckbox.checked
-  };
+      resultsTable.style.display = 'none';
+      resultsContainer.style.display = 'none';
 
-  let isValid = true;
+      location.reload();
+   }
 
-  if (formData.email === '') {
-    displayError(emailInput, 'This field is required.');
-    isValid = false;
-  }
-  if (formData.reEnterEmail === '') {
-    displayError(reEnterEmailInput, 'This field is required.');
-    isValid = false;
-  } else if (formData.email !== formData.reEnterEmail) {
-    displayError(reEnterEmailInput, 'This entry must equal the first entry.');
-    isValid = false;
-  }
+   const requiredFields = [
+      firstNameInput,
+      lastNameInput,
+      cityInput,
+      stateInput,
+      zipInput,
+      grossIncomeInput,
+      ssnInput,
+      applyForCreditCheckbox
+   ];
 
-  for (let i = 0; i < requiredFields.length; i++) {
-    const inputField = requiredFields[i];
-    if (inputField.value === "" || (inputField === applyForCreditCheckbox && !inputField.checked)) {
-      displayError(inputField, "This field is required.");
-      isValid = false;
-    }
-  }
+   function validateForm() {
+      clearErrors();
 
-  const tableBody = resultsTable.querySelector('tbody');
-  for (const field in formData) {
-    const row = tableBody.insertRow();
-    const fieldCell = row.insertCell();
-    const valueCell = row.insertCell();
+      resultsTable.querySelector('tbody').innerHTML = '';
 
-    fieldCell.textContent = field;
+      const formData = {
+         email: emailInput.value.trim(),
+         reEnterEmail: confirmEmailInput.value.trim(),
+         firstName: firstNameInput.value.trim(),
+         lastName: lastNameInput.value.trim(),
+         city: cityInput.value.trim(),
+         state: stateInput.value.trim(),
+         zip: zipInput.value.trim(),
+         grossIncome: parseFloat(grossIncomeInput.value.trim()),
+         ssn: ssnInput.value.trim(),
+         applyForCredit: applyForCreditCheckbox.checked
+      };
 
-    if (formData[field] === '' || (field === 'reEnterEmail' && (formData.email !== formData.reEnterEmail || formData.email === ''))
-      || (field === 'grossIncome' && isNaN(formData[field]) || (field === 'applyForCredit' && !formData[field]))) {
-      valueCell.textContent = 'Invalid Entry';
-      valueCell.classList.add('error');
-      isValid = false;
-    } else {
-      valueCell.textContent = formData[field];
-    }
-  }
+      let isValid = true;
 
-  resultsTable.style.display = 'table';
-  resultsContainer.style.display = 'block'; 
-
-  if (isValid) {
-    setTimeout(() => {
-      if (formData.grossIncome >= 20000) {
-        alert("Congratulations, You are qualified for credit line. A credit card will be sent to you in the mail");
-      } else {
-        alert("We're sorry, you do not qualify for a credit line at this time");
+      if (formData.email === '') {
+         displayError(emailInput, 'This field is required.');
+         isValid = false;
       }
-    }, 500);
-  }
+      if (formData.reEnterEmail === '') {
+         displayError(confirmEmailInput, 'This field is required.');
+         isValid = false;
+      } else if (formData.email !== formData.reEnterEmail) {
+         displayError(confirmEmailInput, 'This entry must equal the first entry.');
+         isValid = false;
+      }
 
-  return isValid;
-}
+      for (let i = 0; i < requiredFields.length; i++) {
+         const inputField = requiredFields[i];
+         if (inputField.value === "" || (inputField === applyForCreditCheckbox && !inputField.checked)) {
+            displayError(inputField, "This field is required.");
+            isValid = false;
+         }
+      }
 
-applyButton.addEventListener('click', (event) => {
-  event.preventDefault();
-  validateForm();
-});
+      const tableBody = resultsTable.querySelector('tbody');
+      for (const field in formData) {
+         const row = tableBody.insertRow();
+         const fieldCell = row.insertCell();
+         const valueCell = row.insertCell();
 
-resetButton.addEventListener('click', () => {
-  emailInput.value = '';
-  reEnterEmailInput.value = '';
-  firstNameInput.value = '';
-  lastNameInput.value = '';
-  cityInput.value = '';
-  stateInput.value = '';
-  zipInput.value = '';
-  grossIncomeInput.value = '';
-  ssnInput.value = '';
-  applyForCreditCheckbox.checked = false;
+         fieldCell.textContent = field;
 
-  clearErrors();
+         if (formData[field] === '' || (field === 'reEnterEmail' && (formData.email !== formData.reEnterEmail || formData.email === ''))
+            || (field === 'grossIncome' && isNaN(formData[field]) || (field === 'applyForCredit' && !formData[field]))) {
+            valueCell.textContent = 'Invalid Entry';
+            valueCell.classList.add('error');
+            isValid = false;
+         } else {
+            valueCell.textContent = formData[field];
+         }
+      }
 
-  resultsTable.innerHTML = '';
-  
-  resultsTable.style.display = 'none';
-  resultsContainer.style.display = 'none';
+      resultsTable.style.display = 'table';
+      resultsContainer.style.display = 'block';
 
-  location.reload();
+      if (isValid) {
+         setTimeout(() => {
+            if (formData.grossIncome >= 20000) {
+               alert("Congratulations, You are qualified for credit line. A credit card will be sent to you in the mail");
+            } else {
+               alert("We're sorry, you do not qualify for a credit line at this time");
+            }
+         }, 500);
+      }
+
+      return isValid;
+   }
+
+   applyButton.addEventListener('click', (event) => {
+      event.preventDefault();
+      validateForm();
+   });
+
+   resetButton.addEventListener('click', () => {
+      emailInput.value = '';
+      reEnterEmailInput.value = '';
+      firstNameInput.value = '';
+      lastNameInput.value = '';
+      cityInput.value = '';
+      stateInput.value = '';
+      zipInput.value = '';
+      grossIncomeInput.value = '';
+      ssnInput.value = '';
+      applyForCreditCheckbox.checked = false;
+
+      clearErrors();
+
+      resultsTable.innerHTML = '';
+
+      resultsTable.style.display = 'none';
+      resultsContainer.style.display = 'none';
+
+      location.reload();
+   });
 });
